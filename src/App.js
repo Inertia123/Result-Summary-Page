c895803d13423d0eb86c5a5c12593d53180cd0e4
Inertia123 <gopal.krushna2002@gmail.com>
2024-01-26 20:59:24 +0530

    finsished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import data from "./data.json";
 
-function App() {
+export default function App(){
+  const [items] = useState(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <main>
+      <section>
+      <div className="top">
+        <h2 className="your">Your Result</h2>
+        <div className="bigcircle">
+          <div className="circle">
+            <span>76</span>
+            <p>of 100</p>
+          </div>
+        </div>
+        <h1>Great</h1>
+        <p className="bpara">You scored higher than 65% of the people who have taken these tests.</p>
+      </div>
+      <div className="bottom">
+        <h2>Summary</h2>
 
-export default App;
+        <div>
+          {items.map((item, index) =>(
+            <div key={index} style={
+              {backgroundColor: `${item.color}10`, display: "flex", justifyContent: "space-between", alignItems: "center", padding: "1rem 2rem", borderRadius: "0.5rem", marginTop: "1rem"}
+            }>
+              <div className="left">
+                <img src={item.icon} alt="image"></img>
+                <h2 style={{color: item.color}}>{item.category}</h2>
+              </div>
+              <p className="hundred"><span className="score">{item.score}</span> / 100</p>
+            </div>
+          ))}
+        </div>
+        <button>Continue</button>
+      </div>
+    </section>
+    </main>
+  )
+}
